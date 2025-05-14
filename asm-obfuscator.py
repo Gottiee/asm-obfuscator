a7c168a8bc93311e4a52e01d693477de6dedb20b
gottiee <gottiedev@gmail.com>
2025-05-14 04:54:45 -0400

    read + create

---
+++ asm-obfuscator.py
@@ -0,0 +1,24 @@
+#!/bin/python3
+
+# pas oublier de skipe les lignes qui commence par ; et split les lignes avec ; et recuper le premier index (pour enlever les commentaires)
+
+import sys
+from src.utils import *
+
+def obf_line(file:str, new_file):
+    for line in file:
+        new_file.write(line)
+
+
+def main(argv, argc):
+    if argc != 2:
+        print_usage()
+        return
+    file = open_file(argv[1])
+    new_file = create_file()
+    obf_line(file, new_file)
+    new_file.close()
+
+
+if __name__ == "__main__":
+    main(sys.argv, len(sys.argv))
---
+++ src/utils.py
@@ -0,0 +1,11 @@
+def print_usage():
+    print("Usage: ./asm-obfuscator.py \"file_to_obf.asm\"")
+    print("quit")
+
+def open_file(file_name:str):
+    with open(file_name, "r") as file:
+        return file.read()
+
+def create_file():
+    f = open("asm/obf.asm", "w")
+    return f
